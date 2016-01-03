b5dd8aef340d83115fa01aa1417361d54f6b9a0f
chengjf <sharpkingdom@gmail.com>
2016-01-03 12:00:02 +0800

    添加sql脚本

---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `gendar` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
